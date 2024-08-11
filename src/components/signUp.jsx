d36ef617567906bcd8bb8b0dfb2385f02e0b6a72
Medhansh Jain <medhanshjain2010@gmail.com>
2024-08-11 12:03:30 +0530

    fix: add missing semicolon

--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -33,7 +33,7 @@ export default function SignUp() {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             alt="Your Company"
-            src="../../public/logo.jpg"
+            src="./logo.jpg"
             className="mx-auto h-[100px]"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
